b08083bf527767d100c8a26fa6939096a96236b7
Melissa Molina <melissa.mm.itt@gmail.com>
2022-01-12 12:06:23 -0500

    footer component done

---
+++ client/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => (
+  <div>
+      <div>
+        <p>Contact info</p>
+        <a href="https://github.com/Dilithium-Crystal-Pistols/graceshopperteam9">Grace Shopper Team 9 GitHub</a>
+      </div>
+    <hr />
+  </div>
+);
+
+export default Footer;
