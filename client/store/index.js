19fb9bb4d70577a4788c78d8b48a4380f3602328
Yvette Gong <yvettethea.gong@gmail.com>
2022-01-18 11:28:54 -0500

    fixing on last poster to product thing

--- client/store/index.js
+++ client/store/index.js
@@ -3,18 +3,13 @@ import {createLogger} from 'redux-logger'
 import thunkMiddleware from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import auth from './auth'
-<<<<<<< HEAD
 import products from './products'
 import singleProduct from './singleProduct'
-
-const reducer = combineReducers({ auth,products, singleProduct })
-=======
-import posters from './posters'
-import singlePoster from './singlePoster'
 import admin from './admin'
 
-const reducer = combineReducers({ auth,posters, singlePoster, admin })
->>>>>>> 095ce459ad257a1963a8f0babb1ade9d8d498067
+const reducer = combineReducers({ auth,products, singleProduct, admin })
+
+
 const middleware = composeWithDevTools(
   applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
 )
@@ -22,11 +17,6 @@ const store = createStore(reducer, middleware)
 
 export default store
 export * from './auth'
-<<<<<<< HEAD
 export * from './products'
 export * from './singleProduct'
-=======
-export * from './posters'
-export * from './singlePoster'
 export * from './admin'
->>>>>>> 095ce459ad257a1963a8f0babb1ade9d8d498067
