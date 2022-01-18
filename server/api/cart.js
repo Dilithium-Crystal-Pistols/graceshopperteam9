5ba61364e16d3cd6f50bb7b19e6fe97049847f89
Yvette Gong <yvettethea.gong@gmail.com>
2022-01-18 13:08:59 -0500

    working on cart backend routes

--- server/api/cart.js
+++ server/api/cart.js
@@ -1,20 +1,45 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-const Cart = require('../db/models/Cart');
-const CartItem = require('../db/models/CartItem');
-const User = require('../db/models/User')
-router.get('/', async(req, res) => {
-    try {
-        //how do we access the user ??
-        //Need a find or create request to get said user's cart
-        // const userId = await User.findOrCreate()
-        // console.log(user)
-        // const userCart = User.getCart();
-        // const cart = await Cart.getItems();
-        // res.send(cart)
-    } catch(error) {
-        console.log(error)
-    }
-})
+const Cart = require("../db/models/Cart");
+const CartItem = require("../db/models/CartItem");
+const Product = require("../db/models/Product");
+const User = require("../db/models/User");
 
-module.exports = router
+//Route for when user clicks his/her cart
+//price, name, quantity
+router.get("/:cartId", async (req, res) => {
+  try {
+    const cart = await Cart.findByPk(req.params.cartId);
+    const cartItems = await CartItem.findAll({
+      where: {
+        cartId: cart.id,
+      },
+    });
+    const products = await Product.findAll({
+      include: [
+        {
+          model: Cart,
+          where: {
+            id: cart.id,
+          },
+        },
+      ],
+    });
+    // const currentCart = {
+    //     userId: cart.userId,
+    //     products: products,
+    //     cartItems: cartItems
+    // }
+
+    //console.log("finalCart: ", currentCart);
+
+    console.log(products[0].product)
+    //const products = await cart.getProducts();
+    //console.log("PRODUCTS: ", products[0].product)
+    //console.log("CART PROTOTYPE: ", Cart.prototype)
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+module.exports = router;
